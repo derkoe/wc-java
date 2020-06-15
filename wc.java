7b0345bd512f3af677db3b519c0cb350c314614a
Christian Köberl <christian.koeberl@gmail.com>
2020-06-15 23:33:48 +0200

    change to long, modify printing

--- wc.java
+++ wc.java
@@ -3,11 +3,12 @@ import java.nio.file.*;
 
 class wc {
     public static void main(String[] args) throws IOException {
-        int lineCount = 0, wordCount = 0, charCount = 0, count;
+        long lineCount = 0, wordCount = 0, charCount = 0;
         char[] cbuf = new char[16 * 1024];
         InputStream is = args.length == 0 ? System.in : Files.newInputStream(Paths.get(args[0]));
         try (Reader reader = new InputStreamReader(is)) {
             boolean prevWhitespace = true;
+            int count;
             while ((count = reader.read(cbuf)) >= 0) {
                 for (int i = 0; i < count; i++) {
                     char charVal = cbuf[i];
@@ -26,7 +27,7 @@ class wc {
         }
 
         String fileName = args.length > 0 ? " " + args[0] : "";
-        System.out.printf("%7d %7d %7d %s\n", lineCount, wordCount, charCount, fileName);
+        System.out.printf("%6d %6d %6d %s\n", lineCount, wordCount, charCount, fileName);
     }
 
     private static boolean isWhitespace(int charVal) {
